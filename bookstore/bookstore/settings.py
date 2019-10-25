bc5e940c115141fd60be58ec1dee7da0ab85adb5
temo881030 <temo881030@gmail.com>
2019-10-25 15:07:22 +0800

    Chapter 5 finished

--- bookstore/bookstore/settings.py
+++ bookstore/bookstore/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'h+*a+8f30uz*ri*vl=uwi8&^lha)q4dqzl&mnfru1_ko*h2x(n'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [""]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main',
 ]
 
 MIDDLEWARE = [
